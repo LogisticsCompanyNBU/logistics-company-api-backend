51ab40af7ff41af375ed44369b8cebfd4acdb151
Encho Belezirev <encho.belezirev@lights.digital>
2023-01-23 08:54:48 +0200

    Extract common user operations and add company client operations

---
+++ src/main/java/org/nbu/company/client/rest/api/CompanyClientsApi.java
@@ -0,0 +1,35 @@
+package org.nbu.company.client.rest.api;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.nbu.company.client.model.Client;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface CompanyClientsApi {
+    @PostMapping(value = "/api/companies/{companyId}/clients", consumes = { "application/json" }, produces = { "application/json" })
+    ResponseEntity<Client> createCompanyClient(@PathVariable("companyId") int companyId, @Valid @RequestBody Client client);
+
+    @DeleteMapping("/api/companies/{companyId}/clients/{clientId}")
+    ResponseEntity<Void> deleteCompanyClientById(@PathVariable("companyId") int companyId, @PathVariable("clientId") int clientId);
+
+    @GetMapping(value = "/api/companies/{companyId}/clients/{clientId}", produces = { "application/json" })
+    ResponseEntity<Client> getCompanyClientById(@PathVariable("companyId") int companyId, @PathVariable("clientId") int clientId);
+
+    @PatchMapping(value = "/api/companies/{companyId}/clients/{clientId}", produces = { "application/json" }, consumes = {
+        "application/json" })
+    ResponseEntity<Client> updateCompanyClientById(@PathVariable("companyId") int companyId, @PathVariable("clientId") int clientId,
+                                                   @Valid @RequestBody Client client);
+
+    @GetMapping(value = "/api/companies/{companyId}/clients", produces = { "application/json" })
+    ResponseEntity<List<Client>> getAllCompanyClients(@PathVariable("companyId") int companyId);
+}
--- src/main/java/org/nbu/company/employee/persistence/EmployeeRepository.java
+++ src/main/java/org/nbu/company/employee/persistence/EmployeeRepository.java
@@ -1,16 +1,10 @@
 package org.nbu.company.employee.persistence;
 
-import java.util.List;
-
 import org.nbu.company.employee.model.Employee;
-import org.nbu.company.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.nbu.shared.UserRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepository extends UserRepository<Employee> {
 
-    Employee findByEmailAndCompany(String email, Company company);
-
-    List<Employee> findAllByCompany(Company company);
 }
